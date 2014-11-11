cdad9fba1df7829cf99df462da6cfacf8629ac9f
Marcus Ibsch <marcus.ibsch@m4ggus.de>
2014-11-11 22:54:10 +0100

    configure flags updated for oxid requirements

--- bin/php-config.sh
+++ bin/php-config.sh
@@ -51,47 +51,93 @@ echo $PHP_CONFIGURE_COMMAND
 
 echo "Press CTRL-C to cancel or ENTER to continue ..."
 
-#if ! read -s; then
-#    echo "aborting PHP $PHP_VERSION configure.."
-#    exit 255
-#fi
+if ! read -s; then
+    echo "aborting PHP $PHP_VERSION configure.."
+    exit 255
+fi
+
+#./configure \
+#    --prefix=/opt/php/${PHP_BASE} \
+#    --with-config-file-path=/opt/php/${PHP_BASE}/etc \
+#    --with-zlib-dir=/usr \
+#	--with-freetype-dir=/usr \
+#    --with-libdir=/lib/x86_64-linux-gnu \
+#    --with-jpeg-dir=/usr \
+#    --with-openssl-dir=/usr \
+#    --with-png-dir=/usr \
+#    --with-xpm-dir=/usr \
+#    --disable-short-tags \
+#    --enable-cgi \
+#    --with-mysql=mysqlnd \
+#    --with-mysqli=mysqlnd \
+#    --with-tidy=/usr \
+#    --with-curl=/usr/bin \
+#    --with-pdo-mysql=mysqlnd \
+#    --with-xsl=/usr \
+#    --with-ldap \
+#    --with-xmlrpc \
+#	--with-openssl \
+#    --with-iconv-dir=/usr \
+#    --with-snmp=/usr \
+#    --enable-sockets \
+#	--enable-sysvsem \
+#	--enable-sysvshm \
+#    --enable-exif \
+#    --enable-ftp \
+#	--enable-intl \
+#	--enable-dba \
+#	--enable-gd-native-ttf \
+#    --enable-calendar \
+#    --with-bz2=/usr \
+#    --with-mcrypt=/usr \
+#    --with-gd \
+#    --enable-mbstring \
+#    --enable-zip \
+#    --with-pear \
+#	--enable-gd-native-ttf
 
 ./configure \
     --prefix=/opt/php/${PHP_BASE} \
     --with-config-file-path=/opt/php/${PHP_BASE}/etc \
-    --with-zlib-dir=/usr \
-	--with-freetype-dir=/usr \
-    --with-libdir=/lib/x86_64-linux-gnu \
-    --with-jpeg-dir=/usr \
-    --with-openssl-dir=/usr \
-    --with-png-dir=/usr \
-    --with-xpm-dir=/usr \
-    --disable-short-tags \
-    --enable-cgi \
-    --with-mysql=mysqlnd \
-    --with-mysqli=mysqlnd \
-    --with-tidy=/usr \
-    --with-curl=/usr/bin \
-    --with-pdo-mysql=mysqlnd \
-    --with-xsl=/usr \
-    --with-ldap \
-    --with-xmlrpc \
-	--with-openssl \
-    --with-iconv-dir=/usr \
-    --with-snmp=/usr \
-    --enable-sockets \
-	--enable-sysvsem \
-	--enable-sysvshm \
-    --enable-exif \
-    --enable-ftp \
-	--enable-intl \
-	--enable-dba \
-	--enable-gd-native-ttf \
-    --enable-calendar \
-    --with-bz2=/usr \
-    --with-mcrypt=/usr \
-    --with-gd \
+    --with-pdo-pgsql \
+    --with-zlib-dir \
+    --with-freetype-dir \
     --enable-mbstring \
+    --with-libxml-dir=/usr \
+    --enable-soap \
+    --enable-calendar \
+	--enable-bcmath \
+    --with-curl \
+    --with-mcrypt \
+    --with-zlib \
+    --with-gd \
+    --with-pgsql \
+    --disable-rpath \
+    --enable-inline-optimization \
+    --with-bz2 \
+    --with-zlib \
+    --enable-sockets \
+    --enable-sysvsem \
+    --enable-sysvshm \
+    --enable-pcntl \
+    --enable-mbregex \
+    --with-mhash \
     --enable-zip \
-    --with-pear \
-	--enable-gd-native-ttf
+    --with-pcre-regex \
+    --with-mysql \
+    --with-pdo-mysql \
+    --with-mysqli \
+    --with-png-dir=/usr \
+    --enable-gd-native-ttf \
+    --with-openssl \
+    --with-libdir=/lib/x86_64-linux-gnu \
+    --enable-ftp \
+    --with-kerberos \
+    --with-gettext \
+    --with-gd \
+    --with-jpeg-dir=/usr/ \
+    --with-png-dir=/usr \
+    --enable-cgi > /dev/null
+
+#    --with-imap \
+#    --with-imap-ssl \
